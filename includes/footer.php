aa845063749fc104bbdd05b160574bcf1c7de163
Matthew-K310 <kennedymatthew310@gmail.com>
2025-02-07 18:39:36 -0600

    commit 2-6-25 @ 1839

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+<footer>
+  <div class="footer">
+    <!--<a href="https://www.instagram.com/matthewikennedy" id="instagramlink">Instagram</a>-->
+    <!--<a href="https://www.youtube.com/channel/UCoqsSKaBEk9B9bblfg_bLsw" id="youtubelink">YouTube</a>-->
+    <!--<a href="https://www.linkedin.com/in/matthew-kennedy-54b410288/" id="linkedinlink">LinkedIn</a>-->
+    <p>&copy; 2025 Belle's Crafts. All Rights Reserved.</p>
+  </div>
+</footer>
+</body>
+
+</html>
